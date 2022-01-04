f63e309564787307a4e244d7268e9c6ddf1d8434
iagoAguiar <iagoaguiar202@gmail.com>
2022-01-04 19:31:42 -0300

    first commit

---
+++ src/main/java/com/avanade/dio/jwt/data/UserData.java
@@ -0,0 +1,11 @@
+package com.avanade.dio.jwt.data;
+
+import java.io.Serializable;
+
+public class UserData implements Serializable {
+
+    private String userName;
+    private String password;
+
+
+}
